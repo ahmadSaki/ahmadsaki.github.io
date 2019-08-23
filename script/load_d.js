77c16bd5c7442ecb20fdae1dc367ed41d01e60ec
saki <saki.virtual@gmail.com>
2019-08-24 03:59:06 +0600

    did not use personal_project.html, dynamic page reload with current state, added json & photo resource for personal project

--- script/load_d.js
+++ script/load_d.js
@@ -1,25 +1,57 @@
 $(document).ready(function () {
 
+
+
+
+    /*
+    http://www.refulz.com:8082/index.php#tab2?foo=789
+    
+    Property    Result
+    ------------------------------------------
+    host        www.refulz.com:8082
+    hostname    www.refulz.com
+    port        8082
+    protocol    http:
+    pathname    index.php
+    href        http://www.refulz.com:8082/index.php#tab2
+    hash        #tab2
+    search      ?foo=789
+    
+    var x = $(location).attr('<property>');
+    */
+    // Current browsing menu wise reload page form loading
+    var current_view = $(location).attr('hash');
+
+    if (current_view == "#portfolio") {
+        jQuery("#embed_form").load("form/portfolio/portfolio.html");
+    }
+    else if (current_view == "#contact") {
+        jQuery("#embed_form").load("form/contact/contact.html");
+    }
+    else {
+        jQuery("#embed_form").load("form/home.html");
+    }
+
+
+
     // Load header,menu & footer into index.html
-    $(function () {
-        jQuery("#embed_header").load("form/header.html");
-        jQuery("#embed_menu").load("form/menu.html");
-        jQuery("#embed_footer").load("form/footer.html");
-    });
+    jQuery("#embed_header").load("form/header.html");
+    jQuery("#embed_menu").load("form/menu.html");
+    jQuery("#embed_footer").load("form/footer.html");
 
 
     // Based on menu load form into index.html
     $("#embed_menu").on("click", "#menu a", function () {
+
         var manuname = $(this).find('li').text();
-        if (manuname == "Portfolio") {
-            $(function () {
-                jQuery("#embed_form").load("form/portfolio/portfolio.html");
-            });
+        if (manuname == "Home") {
+            jQuery("#embed_form").load("form/home.html");
+        }
+        else if (manuname == "Portfolio") {
+            jQuery("#embed_form").load("form/portfolio/portfolio.html");
         }
         else if (manuname == "Contact") {
-            $(function () {
-                jQuery("#embed_form").load("form/contact/contact.html");
-            });
+            jQuery("#embed_form").load("form/contact/contact.html");
         }
     });
 
--- script/portfolio.js
+++ script/portfolio.js
@@ -1,12 +1,23 @@
-$("#embed_form").on("click", "#personal_project a", function () {
-    // alert($(this).attr('value'));
-    $('#personal_project_view').modal('show');
+$.getScript("script/json_pull_connection.js");
 
-    var appTitle = $(this).attr('value');   
-    $("#personal_project_view").find("#app-title").text(appTitle);
+//  personal_project_view Modal
+$("#embed_form").on("click", ".personal_project_list a", function () {
+
+  var appID = $(this).attr('id');  //$(this).get(0).id
+  //console.log(appID);
+  var appTitle = $(this).text();
+
+  ayJSON("/resource/portfolio/personal_project/personal_project.json", function (text) {
+    var data = JSON.parse(text);
+    console.log(data);
+  });
+
+  $("#personal_project_view").find("#app-title").text(appTitle);
+  $('#personal_project_view').modal('show');
 });
 
 
+//  Include all the elements of personal_project.html into portfolio.html
 function embed_personal_project() {
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
@@ -18,10 +29,10 @@ function embed_personal_project() {
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("personal_project_html");
           embed_personal_project();
